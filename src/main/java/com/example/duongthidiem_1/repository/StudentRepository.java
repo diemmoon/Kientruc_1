38d320c7edc6707eae2319101772dc2b2447e77a
diemmoon <duongdiem20@gmail.com>
2021-09-14 16:10:32 +0700

    commit jpql

--- src/main/java/com/example/duongthidiem_1/repository/StudentRepository.java
+++ src/main/java/com/example/duongthidiem_1/repository/StudentRepository.java
@@ -2,6 +2,28 @@ package com.example.duongthidiem_1.repository;
 
 import com.example.duongthidiem_1.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface StudentRepository  extends JpaRepository<Student, Integer> {
+    @Query("select s from Student s")
+    List<Student> getAll();
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into student(first_name,last_name,email) values(?1,?2,?3)", nativeQuery = true)
+    void addStudent(String firstName, String lastName, String email);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update Student s set s.firstName=?2, s.lastName=?3, s.email = ?4 where s.id = ?1")
+    void updateStudent(int id ,String firstName, String lastName, String email);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Student s where s.id = ?1")
+    void deleteStudent(int id);
 }
--- src/test/java/com/example/duongthidiem_1/DuongThiDiem1ApplicationTests.java
+++ src/test/java/com/example/duongthidiem_1/DuongThiDiem1ApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.duongthidiem_1;
 
+import com.example.duongthidiem_1.entity.Student;
+import com.example.duongthidiem_1.repository.StudentRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DuongThiDiem1ApplicationTests {
+    @Autowired
+    private StudentRepository studentRepository;
+    @Test
+    public void getAll() {
+        System.out.println("getAll: " + studentRepository.getAll());
+    }
+    @Test
+    public void add() {
+        studentRepository.addStudent("Duong ", "Diem", "diem@gmail.com");
+    }
+    @Test
+    public void update() {
+        Student student = studentRepository.findById(3).get();
+        studentRepository.updateStudent(3, "Moon ", "hihi", "kk@gmail.com");
+    }
 
     @Test
-    void contextLoads() {
+    public void delete() {
+
+        studentRepository.deleteStudent(3);
     }
 
 }
